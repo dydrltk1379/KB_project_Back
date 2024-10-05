07534a2f0afbcb2cf8bd9f5c0013cf1a88a6f41f
gyeommm <96188162+gyeommm@users.noreply.github.com>
2024-10-05 17:58:58 +0900

    Revert "10-04 -이규빈"

--- src/main/java/com/finns/post/service/PostService.java
+++
@@ -1,8 +0,0 @@
-package com.finns.post.service;
-
-import com.finns.post.dto.PostDTO;
-
-public interface PostService {
-    PostDTO getPostById(Long id);
-    void updatePost(Long id, PostDTO postDTO);
-}
