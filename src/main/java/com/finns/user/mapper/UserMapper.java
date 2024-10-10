53a402ec97435f6e4a940d91fbd69ca6153fcd8c
ingyeom <wjd1836@gmail.com>
2024-10-10 18:40:18 +0900

    Feat: 소비 TOP 3명 뽑기, 나와 같은 mbti 유저 최대 5명 뽑기, 마지막 갱신 날짜 표시 기능 구현

--- src/main/java/com/finns/user/mapper/UserMapper.java
+++ src/main/java/com/finns/user/mapper/UserMapper.java
@@ -1,11 +1,14 @@
 package com.finns.user.mapper;
 
 import com.finns.post.dto.ChangeRenewStatusDTO;
-import com.finns.user.dto.SetMbtiDTO;
-import com.finns.user.dto.User;
+import com.finns.user.dto.*;
+
+import java.util.List;
 
 public interface UserMapper {
     User selectOne(Long userNo);
+    List<UserTop3DTO> selectTop3ForAmountByDate(YearAndMonthDTO yearAndMonthDTO);
+    List<UserRecommendResponseDTO> selectRecommend5ByMbti(UserRecommendRequestDTO userRecommendRequestDTO);
     void updateRenewTime(ChangeRenewStatusDTO changeRenewStatusDTO);
     void updateMbti(SetMbtiDTO setMbtiDTO);
 }
