f6804feda4144740a9b778e3a8a760e95e1c24e3
dydrltk <dydrltk1379@gmail.com>
2024-10-10 22:15:53 +0900

    Feat : 예ë금, 적금 은행로고 이미지 추가

--- src/main/java/com/finns/finance/controller/FinanceController.java
+++ src/main/java/com/finns/finance/controller/FinanceController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Api(value = "FinanceController", tags = "금융상품 정보")
 @PropertySource({"classpath:/application.properties"})
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
 public class FinanceController {
 
     private final FinanceService financeService;
